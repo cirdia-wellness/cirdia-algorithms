ba3539edd443c3fdee60f273a7adb40b7509aba5
NYBACHOK <nybachokua@gmail.com>
2025-06-27 17:58:23 +0300

    save

--- period/src/lib.rs
+++ period/src/lib.rs
@@ -1,16 +1,24 @@
-use std::{collections::BTreeMap, time::Duration};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    time::Duration,
+};
+
+mod normal_f64;
+
+pub use self::normal_f64::*;
 
 const DAY: Duration = Duration::new(86400, 0);
+const PERCENT_FOR_TEMPERATURE: f64 = 0.25;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Record {
-    pub temperature: f64,
+    pub temperature: NormalF64,
     pub heart_rate_variability: Duration,
     pub timestamp: Duration,
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum PeriodStage {
     PreOvulation,
@@ -27,32 +35,13 @@ pub struct Period {
     pub kind: PeriodStage,
 }
 
-#[derive(PartialEq, PartialOrd)]
-struct F64Wrapper(f64);
-
-impl Eq for F64Wrapper {}
-
-impl Ord for F64Wrapper {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // Algorithms assumes that all values are normal and could be compared. Anyway I think it's fine to consider NaN and NaN equal in this case
-        self.0
-            .partial_cmp(&other.0)
-            .unwrap_or(std::cmp::Ordering::Equal)
-    }
-}
-
-impl From<F64Wrapper> for f64 {
-    fn from(F64Wrapper(value): F64Wrapper) -> Self {
-        value
-    }
-}
-
+#[derive(Clone)]
 enum DataPoint {
     Start {
         start_timestamp: Duration,
         end_timestamp: Duration,
     },
-    MiddleUnspecified {
+    MiddleUnchecked {
         start_timestamp: Duration,
         end_timestamp: Duration,
         temperature_diff: f64,
@@ -64,11 +53,10 @@ enum DataPoint {
     UnknownOrCorruptedData,
 }
 
-// Q:
-// Use activity to filter if yes in app or in algorithm?
-// IS there any issue to compare lowest temperatures instead of avg ?
-// Is sudden drop should be confirmed by next X days?
-pub fn period<R: Into<Record>>(data: impl IntoIterator<Item = R>, base_temperature: f64) {
+pub fn period<R: Into<Record>>(
+    data: impl IntoIterator<Item = R>,
+    base_temperature: f64,
+) -> Vec<Period> {
     let data = data
         .into_iter()
         .map(|this| {
@@ -83,7 +71,7 @@ pub fn period<R: Into<Record>>(data: impl IntoIterator<Item = R>, base_temperatu
         .collect::<BTreeMap<_, _>>();
 
     if data.is_empty() {
-        return;
+        return Vec::new();
     }
 
     let mut start_timestamp = data
@@ -92,7 +80,7 @@ pub fn period<R: Into<Record>>(data: impl IntoIterator<Item = R>, base_temperatu
         .0
         .to_owned();
 
-    let mut period_result = Vec::new();
+    let mut inter_period_result = Vec::new();
 
     let mut day_chunk;
 
@@ -107,28 +95,36 @@ pub fn period<R: Into<Record>>(data: impl IntoIterator<Item = R>, base_temperatu
             break;
         }
 
-        let min_t: f64 = day_chunk
-            .iter()
-            .map(|this| F64Wrapper(this.1.0))
-            .min()
-            .expect("Not empty")
-            .into();
+        let min_t: f64 = {
+            let values = day_chunk
+                .iter()
+                .map(|this| this.1.0)
+                .collect::<BTreeSet<_>>();
+
+            let size = (values.len() as f64 * PERCENT_FOR_TEMPERATURE).floor();
+
+            values
+                .into_iter()
+                .take(size as usize)
+                .map(|this| this.into_inner())
+                .sum::<f64>()
+                / size
+        };
 
         if min_t <= base_temperature {
-            let prev_point = period_result.last();
+            let prev_point = inter_period_result.last();
 
             match prev_point {
-                Some(DataPoint::MiddleUnspecified {
+                Some(DataPoint::MiddleUnchecked {
                     end_timestamp: prev_end_timestamp,
                     ..
                 }) if end_timestamp - *prev_end_timestamp <= DAY => {
-                    // TODO: Should this be day or more?
-                    period_result.push(DataPoint::End {
+                    inter_period_result.push(DataPoint::End {
                         start_timestamp,
                         end_timestamp,
                     })
                 }
-                _ => period_result.push(DataPoint::Start {
+                _ => inter_period_result.push(DataPoint::Start {
                     start_timestamp,
                     end_timestamp,
                 }),
@@ -139,33 +135,66 @@ pub fn period<R: Into<Record>>(data: impl IntoIterator<Item = R>, base_temperatu
 
         let temperature_diff = min_t - base_temperature;
 
-        // TODO: How to process way bigger temperature diff?
         if base_temperature < min_t && temperature_diff <= 0.6 && temperature_diff >= 0.3 {
-            // TODO: HOW TO CONFIRM BY HRV AND SLEEP?
-
-            period_result.push(DataPoint::MiddleUnspecified {
+            inter_period_result.push(DataPoint::MiddleUnchecked {
                 temperature_diff,
                 start_timestamp,
                 end_timestamp,
             });
+        } else {
+            inter_period_result.push(DataPoint::UnknownOrCorruptedData);
         }
 
         start_timestamp = end_timestamp;
     }
 
-    // let mut result = Vec::new();
+    let mut period_result = Vec::<Period>::new();
 
-    for (i, point) in period_result.into_iter().enumerate() {
+    for mut i in 0..inter_period_result.len() {
+        let point = match inter_period_result.get(i) {
+            Some(p) => p.to_owned(),
+            None => break,
+        };
+
+        // Notes for future. Currently method checks for last point to remove possible errors
+        // This should improve precision but ideally it should not only look at last element
+        // but at X elements forward and back.
         match point {
             DataPoint::Start {
                 start_timestamp,
                 end_timestamp,
-            } => todo!(),
-            DataPoint::MiddleUnspecified {
+            } => {
+                let kind = match period_result.last() {
+                    Some(last)
+                        if last.kind == PeriodStage::PreOvulation
+                            || last.kind == PeriodStage::PeriodStart =>
+                    {
+                        PeriodStage::PreOvulation
+                    }
+                    // If previous period was in conflict with current I assume that previous was correct while this
+                    // day was influenced by some external factor that doesn't taken into account now
+                    Some(last) => last.kind,
+                    None => PeriodStage::PreOvulation,
+                };
+
+                period_result.push(Period {
+                    start_timestamp,
+                    end_timestamp,
+                    kind,
+                })
+            }
+            // Case for elevated temperature
+            DataPoint::MiddleUnchecked {
                 start_timestamp,
                 end_timestamp,
                 temperature_diff,
-            } => todo!(),
+            } => {
+                const CHECK_NEXT_INDEXES_OFFSET: [usize; 3] = [1, 2, 3];
+
+                // Check that next 3 days have same or higher temperature diff. Otherwise don't count this
+
+                todo!()
+            }
             DataPoint::End {
                 start_timestamp,
                 end_timestamp,
@@ -174,4 +203,6 @@ pub fn period<R: Into<Record>>(data: impl IntoIterator<Item = R>, base_temperatu
             DataPoint::UnknownOrCorruptedData => continue,
         }
     }
+
+    period_result
 }
